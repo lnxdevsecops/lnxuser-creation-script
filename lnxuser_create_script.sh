ca64dd9c0141bb9ded09c22d185420cabc0f72af
linuxadm-c <118249982+linuxadm-c@users.noreply.github.com>
2024-01-04 23:45:30 +0530

    Update lnxuser_create_script.sh_final

--- lnxuser_create_script.sh
+++ lnxuser_create_script.sh
@@ -7,47 +7,53 @@ RESET='\033[0m' # Reset to default color
 read -p "Please provide user's name by separating white space. Please don't use comma '(,)': " username
 echo " "
 
-for user in $username; do
-    id $user 2>/dev/null 1>/dev/null
-    if [ $? != 0 ]; then
-        new_user_data+=$user
-        new_user_data+=" "
-    else
-        exit_user_data+="$user"
-        exit_user_data+=" "
+if [ -n "$username" ]; then
+
+    for user in $username; do
+        id $user 2>/dev/null 1>/dev/null
+        if [ $? != 0 ]; then
+            new_user_data+=$user
+            new_user_data+=" "
+        else
+            exit_user_data+="$user"
+            exit_user_data+=" "
+        fi
+    done
+
+    # Creating new users
+    echo -e "Creating  New Users.........! \n"
+
+    for new_user in ${new_user_data[@]}; do
+        useradd -m $new_user
+
+        #Generating random password
+        rand_pass=$(openssl rand -base64 14)
+        # setting up the password
+        # echo -e "$rand_pass\n$rand_pass" | passwd $uname 1>/dev/nuall
+        echo "$rand_pass" | passwd --stdin $new_user 1>/dev/null
+
+        if [ -n $rand_pass ]; then
+            password_set="Yes"
+        else
+            password_set="No"
+        fi
+        # Shows user details
+        echo -e "UserName: $new_user \nPassword Status: $password_set \n"
+
+        # stores user and password details in /root/.credentials files
+        echo -e "UserName: $new_user \nPassword: $rand_pass" >>/root/.credentials
+
+        sleep 3
+    done
+
+    if [ -n "$exit_user_data" ]; then
+        echo " "
+        echo -e "${YELLOW}Unable to create users '"$exit_user_data"' as they are already exist.${RESET} \n"
     fi
-done
 
-# Creating new users
-echo -e "Creating  New Users.........! \n"
+    sleep 1
+    echo -e "${RED}Please get user's credentials from /root/.credentials file.${RESET} \n"
 
-for new_user in ${new_user_data[@]}; do
-    useradd -m $new_user
-
-    #Generating random password
-    rand_pass=$(openssl rand -base64 14)
-    # setting up the password
-    # echo -e "$rand_pass\n$rand_pass" | passwd $uname 1>/dev/nuall
-    echo "$rand_pass" | passwd --stdin $new_user 1>/dev/null
-
-    if [ -n $rand_pass ]; then
-        password_set="Yes"
-    else
-        password_set="No"
-    fi
-    # Shows user details
-    echo -e "UserName: $new_user \nPassword Status: $password_set \n"
-
-    # stores user and password details in /root/.credentials files
-    echo -e "UserName: $new_user \nPassword: $rand_pass" >>/root/.credentials
-
-    sleep 3
-done
-
-if [ -n "$exit_user_data" ]; then
-    echo " "
-    echo -e "${YELLOW}Unable to create users '"$exit_user_data"' as they are already exist.${RESET} \n"
+else
+    echo "You haven't provide any user name"
 fi
-
-sleep 1
-echo -e "${RED}Please get user's credentials from /root/.credentials file.${RESET} \n"
